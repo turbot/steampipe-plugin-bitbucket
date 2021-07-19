6d7238f5e150b00778f4f65120cd17cfc44bfca2
Sayan Saha <sayan@turbot.com>
2021-07-19 16:59:41 +0530

    Added pagination in my_repository list call

--- bitbucket/table_bitbucket_my_repository.go
+++ bitbucket/table_bitbucket_my_repository.go
@@ -23,28 +23,33 @@ func tableBitbucketMyRepository(_ context.Context) *plugin.Table {
 func tableBitbucketMyRepositoryList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	owner := h.Item.(bitbucket.Workspace).Slug
 	client := connect(ctx, d)
-
 	urlStr := client.GetApiBaseURL() + fmt.Sprintf("/repositories/%s", owner)
-	resp, err := client.HttpClient.Get(urlStr)
-	if err != nil {
-		if isForbiddenError(err) {
+
+	for {
+		resp, err := client.HttpClient.Get(urlStr)
+		if err != nil {
+			if isForbiddenError(err) {
+				return nil, nil
+			}
+			if isNotFoundError(err) {
+				return nil, nil
+			}
+			plugin.Logger(ctx).Error("tableBitbucketRepositoryList", "Error", err)
+			return nil, err
+		}
+
+		repositoryList := new(RepositoryList)
+		err = decodeResponse(resp, repositoryList)
+		if err != nil {
+			return nil, err
+		}
+		for _, repository := range repositoryList.Repositories {
+			d.StreamListItem(ctx, repository)
+		}
+		if repositoryList.Next == "" {
 			return nil, nil
 		}
-		if isNotFoundError(err) {
-			return nil, nil
-		}
-		plugin.Logger(ctx).Error("tableBitbucketRepositoryList", "Error", err)
-		return nil, err
+		// update urlstring with the link of next page
+		urlStr = repositoryList.Next
 	}
-
-	repository := new(RepositoryList)
-	err = decodeResponse(resp, repository)
-	if err != nil {
-		return nil, err
-	}
-	for _, repo := range repository.Repositories {
-		d.StreamListItem(ctx, repo)
-	}
-
-	return nil, nil
 }
