3e02e365192022b86a77f541a55c72576a8c19a6
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2021-07-20 14:12:09 +0530

    Add table bitbucket_branch. Closes #19 (#36)

---
+++ bitbucket/table_bitbucket_branch.go
@@ -0,0 +1,144 @@
+package bitbucket
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ktrysmt/go-bitbucket"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+func tableBitbucketBranch(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "bitbucket_branch",
+		Description: "BitBucket Branch.",
+		List: &plugin.ListConfig{
+			KeyColumns: plugin.SingleColumn("repository_full_name"),
+			Hydrate:    tableBitbucketBranchesList,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.AllColumns([]string{"repository_full_name", "name"}),
+			Hydrate:    tableBitbucketBranchGet,
+		},
+		Columns: []*plugin.Column{
+			// top fields
+			{
+				Name:        "name",
+				Description: "The name of the branch.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "repository_full_name",
+				Description: "The repository full name.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromQual("repository_full_name"),
+			},
+			{
+				Name:        "type",
+				Description: "The branch type.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "default_merge_strategy",
+				Description: "Branch default merge strategy.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Default_Merge_Strategy"),
+			},
+
+			// json fields
+			{
+				Name:        "heads",
+				Description: "Branch head details.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "links",
+				Description: "Branch link details.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "merge_strategies",
+				Description: "Branch merge strategies.",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Merge_Strategies"),
+			},
+			{
+				Name:        "target",
+				Description: "Branch target details.",
+				Type:        proto.ColumnType_JSON,
+			},
+
+			// Standard columns
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Name"),
+			},
+		},
+	}
+}
+
+func tableBitbucketBranchesList(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	repoFullName := d.KeyColumnQuals["repository_full_name"].GetStringValue()
+	owner, repoName := parseRepoFullName(repoFullName)
+
+	client := connect(ctx, d)
+	opts := &bitbucket.RepositoryBranchOptions{
+		Owner:    owner,
+		RepoSlug: repoName,
+	}
+
+	branches, err := client.Repositories.Repository.ListBranches(opts)
+	if err != nil {
+		return nil, err
+	}
+	if branches != nil {
+		for _, branch := range branches.Branches {
+			d.StreamListItem(ctx, branch)
+		}
+	}
+
+	return nil, nil
+}
+
+func tableBitbucketBranchGet(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("tableBitbucketBranchGet")
+
+	repoFullName := d.KeyColumnQuals["repository_full_name"].GetStringValue()
+	name := d.KeyColumnQuals["name"].GetStringValue()
+
+	if repoFullName == "" || name == "" {
+		return nil, nil
+	}
+	owner, repoName := parseRepoFullName(repoFullName)
+
+	if owner == "" || repoName == "" {
+		return nil, fmt.Errorf("repository_full_name should be in the format \"{workspace_slug}/{repo_slug}\"")
+	}
+
+	client := connect(ctx, d)
+
+	opts := &bitbucket.RepositoryBranchOptions{
+		Owner:      owner,
+		RepoSlug:   repoName,
+		BranchName: name,
+	}
+
+	response, err := client.Repositories.Repository.GetBranch(opts)
+	if err != nil {
+		if isNotFoundError(err) {
+			return nil, nil
+		}
+		plugin.Logger(ctx).Error("tableBitbucketBranchGet", "Error", err)
+		return nil, err
+	}
+
+	if response == nil {
+		return nil, nil
+	}
+
+	return response, nil
+}
---
+++ docs/tables/bitbucket_branch.md
@@ -0,0 +1,36 @@
+# Table: bitbucket_branch
+
+A branch represents an independent line of development. Branches serve as an abstraction for the edit/stage/commit process. You can think of them as a way to request a brand new working directory, staging area, and project history. New commits are recorded in the history for the current branch, which results in a fork in the history of the project.
+
+The `bitbucket_branch` table can be used to query branches belonging to a repository, and **you must specify which repository** with `where repository_full_name='workspace/repository'`.
+
+## Examples
+
+### List branches in a repository
+
+```sql
+select
+  name,
+  repository_full_name,
+  default_merge_strategy,
+  type
+from
+  bitbucket_branch
+where
+  repository_full_name = 'souravthe/test1';
+```
+
+### List branches with `merge_commit` default merge strategy
+
+```sql
+select
+  name,
+  repository_full_name,
+  default_merge_strategy,
+  type
+from
+  bitbucket_branch
+where
+  repository_full_name = 'souravthe/test1'
+  and default_merge_strategy = 'merge_commit';
+```
