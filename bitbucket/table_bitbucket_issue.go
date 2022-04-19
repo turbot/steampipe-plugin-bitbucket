69f36749554cdf25454e7a7bdb7a2cd9ec1155e9
misraved <ved@turbot.com>
2022-04-20 02:31:15 +0530

    Update SDK

--- bitbucket/table_bitbucket_issue.go
+++ bitbucket/table_bitbucket_issue.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	"github.com/ktrysmt/go-bitbucket"
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableBitbucketIssue(_ context.Context) *plugin.Table {
--- bitbucket/utils.go
+++ bitbucket/utils.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/ktrysmt/go-bitbucket"
 
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 //// Constants
