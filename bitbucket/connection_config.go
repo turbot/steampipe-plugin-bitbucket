bda385c595e6a184b2f2cf1724868ce1caccaf86
Madhushree Ray <madhushree@turbot.com>
2023-12-12 23:56:52 +0530

    Removed config schema

--- bitbucket/connection_config.go
+++ bitbucket/connection_config.go
@@ -2,25 +2,12 @@ package bitbucket
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type bitbucketConfig struct {
-	Username *string `cty:"username"`
-	Password *string `cty:"password"`
-	BaseUrl  *string `cty:"base_url"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"username": {
-		Type: schema.TypeString,
-	},
-	"password": {
-		Type: schema.TypeString,
-	},
-	"base_url": {
-		Type: schema.TypeString,
-	},
+	Username *string `hcl:"username"`
+	Password *string `hcl:"password"`
+	BaseUrl  *string `hcl:"base_url"`
 }
 
 func ConfigInstance() interface{} {
